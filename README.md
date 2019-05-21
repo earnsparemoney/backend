f93ddd22ff86fdb07ced7df52792b33025320068
873421427 <873421427@qq.com>
2019-05-21 17:12:41 +0800

    update reademe abount how to set up project

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # earn spare money backend
 A go implementation of backend service
+
+
+## how to run
+1. make sure you have install go and configure your go path correctly
+2. download gopm, which is a package manager
+```
+go get -u github.com/gpmgo/gopm
+```
+3. get packages needed in .gopmfile using gopm command 
+```
+gopm get -g
+```
+4. run
+```
+go run main.go
+```
--- controllers/userController.go
+++ controllers/userController.go
@@ -4,10 +4,14 @@ import (
 	//"github.com/earnsparemoney/backend/models"
 	"github.com/labstack/echo"
 	"net/http"
+	"github.com/earnsparemoney/backend/utils"
 )
 
+//get parameters from request body
+//check if some parameters are invalid
+// call model's function to add a new user
 func AddUser(c echo.Context) error{
-	return c.String(http.StatusOK, "hello world")	
+	
 }
 
 func LoginUser(c echo.Context) error{
