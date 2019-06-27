b5eabd4b4fc0ac1cefd2d5bc4e74459370b7e794
zhongwq <1316628630@qq.com>
2019-06-27 20:20:31 +0800

    Delete some config

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
   port: 4000,
   db: {
-    user: process.env.USER || 'root',
-    password: process.env.PASSWORD || 'abc25834934F',
-    database: process.env.DATABASE || 'EarnMyMoney',
+    user: process.env.USER || 'EarnMoney',
+    password: process.env.PASSWORD || 'EarnMoney',
+    database: process.env.DATABASE || 'EarnMoney',
     options: {
       dialect: process.env.DIALECT || 'mysql',
-      host: process.env.HOST || 'localhost',
+      host: process.env.HOST || 'mysql',
       port: 3306,
       define: {
         charset: 'utf8mb4',
